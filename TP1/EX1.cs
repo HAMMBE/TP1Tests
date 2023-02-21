cf19986f498f9353c091638d06db68802099670c
HAMMBE <benjamin.hamm7@gmail.com>
2023-02-21 10:32:57 +0100

    feat: GREEN : Add FIZZBUZZ generer

--- TP1/EX1.cs
+++ TP1/EX1.cs
@@ -4,6 +4,33 @@ public class EX1
 {
     public static string Generer(int n)
     {
-        return null;
+        if(n < 15 || n > 150)
+        {
+            return string.Empty;
+        }
+        
+        string result = string.Empty;
+        
+        for(int i = 1; i <= n ; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                result += "FizzBuzz ";
+            }
+            else if (i % 3 == 0)
+            {
+                result += "Fizz ";
+            }
+            else if (i % 5 == 0)
+            {
+                result += "Buzz ";
+            }
+            else
+            {
+                result += i + " ";
+            }
+        }
+
+        return result;
     }
 }
--- TP1/Program.cs
+++ TP1/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using TP1;
+
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine(EX1.Generer(15));
