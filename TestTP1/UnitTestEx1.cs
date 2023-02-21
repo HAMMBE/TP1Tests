2993a87370e43c5ae1a000be07e16200dfc73f3f
HAMMBE <benjamin.hamm7@gmail.com>
2023-02-21 10:30:36 +0100

    fix: RED fix des tests

--- TestTP1/UnitTestEx1.cs
+++ TestTP1/UnitTestEx1.cs
@@ -4,9 +4,7 @@ namespace TestTP1;
 public class UnitTest1
 {
     [Theory]
-    [InlineData("FizzBuzz", 15)]
-    [InlineData("Fizz", 18)]
-    [InlineData("Buzz", 25)]
+    [InlineData("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz ", 15)]
     [InlineData("", 14)]
     [InlineData("", 151)]
     public void TestFizzBuzz(string expected, int n)
