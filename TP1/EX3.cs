e328452595af3f29f353068bbb20b12fa07c8c6f
HAMMBE <benjamin.hamm7@gmail.com>
2023-02-21 11:18:44 +0100

    REFACTOR : switch in Imposition

--- TP1/EX3.cs
+++ TP1/EX3.cs
@@ -9,25 +9,19 @@ public class EX3
             throw new ArgumentException("Revenu must be positive");
         }
         
-        if (revenu <= 10777)
+        switch (revenu)
         {
-            return "0% - 0";
+            case <= 10777:
+                return "0% - 0";
+            case <= 27478:
+                return "11% - " + (revenu * 0.11);
+            case <= 78570:
+                return "30% - " + (revenu * 0.3);
+            case <= 168994:
+                return "41% - " + (revenu * 0.41);
+            default:
+                return "45% - " + (revenu * 0.45);
         }
-        else if (revenu <= 27478)
-        {
-            return "11% - " + (revenu * 0.11);
-        }
-        else if (revenu <= 78570)
-        {
-            return "30% - " + (revenu * 0.3);
-        }
-        else if (revenu <= 168994)
-        {
-            return "41% - " + (revenu * 0.41);
-        }
-        else
-        {
-            return "45% - " + (revenu * 0.45);
-        }
+        
     }
 }
