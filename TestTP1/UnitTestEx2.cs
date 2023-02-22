35fd3ec8afa605dec63bff641e5e62d76c9abf09
HAMMBE <benjamin.hamm7@gmail.com>
2023-02-22 09:21:58 +0100

    feat: RED : REDO EX2

--- TestTP1/UnitTestEx2.cs
+++ TestTP1/UnitTestEx2.cs
@@ -5,40 +5,29 @@ namespace TestTP1;
 public class UnitTestEx2
 {
     [Theory]
-    [InlineData(0, -1)]
-    [InlineData(-1, 0)]
-    [InlineData(-1, -1)]
-    public void TestNegativeValues(int firstPlayerExchangesWon, int secondPlayerExchangesWon)
+    [InlineData(0, 0, "Zero All")]
+    [InlineData(1, 1, "Fifteen All")]
+    [InlineData(2, 2, "Thirty All")]
+    [InlineData(3, 3, "Deuce")]
+    [InlineData(4, 4, "Deuce")]
+    [InlineData(1, 0, "Fifteen - Zero")]
+    [InlineData(0, 1, "Zero - Fifteen")]
+    [InlineData(2, 0, "Thirty - Zero")]
+    [InlineData(0, 2, "Zero - Thirty")]
+    [InlineData(3, 0, "Forty - Zero")]
+    [InlineData(0, 3, "Zero - Forty")]
+    [InlineData(4, 0, "Game Player 1")]
+    [InlineData(0, 4, "Game Player 2")]
+    [InlineData(4, 3, "Advantage Player 1")]
+    [InlineData(3, 4, "Advantage Player 2")]
+    [InlineData(5, 3, "Game Player 1")]
+    [InlineData(3, 5, "Game Player 2")]
+    [InlineData(6, 4, "Game Player 1")]
+    [InlineData(4, 6, "Game Player 2")]
+    [InlineData(7, 5, "Game Player 1")]
+    [InlineData(5, 7, "Game Player 2")]
+    public void TennisGameTest(int player1Points, int player2Points, string expectedScore)
     {
-        EX2 game = new EX2(firstPlayerExchangesWon, secondPlayerExchangesWon);
-        Assert.Throws<ArgumentException>(() => game.ComputeScore());
+        Assert.Equal(expectedScore, EX2.TennisScore(player1Points, player2Points));
     }
-    
-    [Theory]
-    [InlineData(0, 0, 0, 0)]
-    [InlineData(1, 0, 15, 0)]
-    [InlineData(2, 0, 30, 0)]
-    [InlineData(3, 0, 40, 0)]
-    [InlineData(4, 2, 40, 30)]
-    [InlineData(2, 3, 30, 40)]
-    public void ShouldReturnCorrectScore(int firstPlayerExchangesWon, int secondPlayerExchangesWon, int firstPlayerExpectedScore, int secondPlayerExpectedScore)
-    {
-        EX2 game = new EX2(firstPlayerExchangesWon, secondPlayerExchangesWon);
-        game.ComputeScore();
-        Assert.Equal(firstPlayerExpectedScore, game.FirstPlayerScore);
-        Assert.Equal(secondPlayerExpectedScore, game.SecondPlayerScore);
-    }
-    
-    [Theory]
-    [InlineData(4, 0, EX2.FirstPlayer)]
-    [InlineData(0, 4, EX2.SecondPlayer)]
-    [InlineData(7, 5, EX2.FirstPlayer)]
-    [InlineData(5, 7, EX2.SecondPlayer)]
-    public void ShouldReturnCorrectWinner(int firstPlayerExchangesWon, int secondPlayerExchangesWon, string winner)
-    {
-        EX2 game = new EX2(firstPlayerExchangesWon, secondPlayerExchangesWon);
-        game.ComputeScore();
-        Assert.Equal(winner, game.Winner);
-    }
-
 }
